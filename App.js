7ab2ff8c7cf6105ce7a9d3795074fdc91dda5775
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-05-17 01:34:24 -0300

    Criando estrutura da interface

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView, StatusBar} from 'react-native'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function App(){
+  return(
+    <SafeAreaView style={styles.box}>
+      <StatusBar backgroundColor="#171d33" barStyle="light-content" />
+      <Text>Tasks</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  box: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: '#171d33'
+  }
+})
