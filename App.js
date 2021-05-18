1b9e35e854e882175fc13102749a605affdcdf06
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-05-18 01:49:15 -0300

    Adicionando animaçoes e interaçaoes

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ export default function App() {
   const [task, setTask] = useState([]);
 
   const [openModal, setOpenModal] = useState(false);
+  const [validate, setValidate] = useState(false);
+
   const [inputText, setInputText] = useState('');
 
 
@@ -33,6 +35,11 @@ export default function App() {
     setInputText('');
   }
 
+  function validated() {
+    setOpenModal(true)
+    setValidate(true)
+  }
+
   const deleteTask = useCallback((data) => {
     const find = task.filter(result => result.key !== data.key); /* Compara os o array antigo com o novo */
     setTask(find);
@@ -42,20 +49,46 @@ export default function App() {
   return (
     <SafeAreaView style={styles.box}>
       <StatusBar backgroundColor="#171d33" barStyle="light-content" />
-      <View style={styles.content}>
-        <Text style={styles.titulo}>Lembretes</Text>
-      </View>
 
-      {/* Lista */}
-      <FlatList
-        style={styles.lista}
-        showsHorizontalScrollIndicator={false} /* Desabilita a barra de rolagem */
-        data={task} /* Onde vai todos os itens da lista */
-        keyExtractor={(item) => String(item.key)} /* Chave do item */
-        renderItem={
-          ({ item }) => <TaskList data={item} deleteTask={deleteTask} />  /* Como vai ser renderizado o item */
-        }
-      />
+      {/* Mensagem inicial quando não tem itens cadastrados */}
+      {task.length === 0 && validate !== true &&
+        <View style={styles.contentInitial}>
+          <Ionicons name="clipboard-sharp" size={100} color="lightgreen" />
+          <Text style={styles.welcomeTitulo}>Bem vindo</Text>
+          <Text style={styles.welcomeDescricao}>Nesta plataforma você pode anotar suas rotinas, lembretes, tarefas curtas. Acompanhando sempre suas tarefas direto do seu bolso.</Text>
+
+          <AnimatedButton
+            style={styles.welcomeAdd}
+            animation="bounceInUp"
+            duration={2000}
+            onPress={validated}
+            useNativeDriver>
+            <Ionicons name="ios-add" size={30} color="#171d33" />
+          </AnimatedButton>
+
+        </View>
+      }
+
+
+      {/* Mensagem inicial quando não tem itens cadastrados */}
+      {task.length === 0 && validate !== false &&
+        <View style={styles.contentInitial}>
+          <Ionicons name="search-outline" size={100} color="lightgreen" />
+          <Text style={styles.welcomeTitulo}>Lista vazia</Text>
+          <Text style={styles.welcomeDescricao}>Vimos que sua lista está vazia, clique em adicionar tarefas para criar uma lista.</Text>
+
+          <AnimatedButton
+            style={styles.addTask}
+            animation="bounceInUp"
+            duration={2000}
+            onPress={validated}
+            useNativeDriver>
+            <Text>Adicionar tarefas</Text>
+          </AnimatedButton>
+
+        </View>
+      }
+
 
       <Modal animationType="slide" transparent={false} visible={openModal}>
         <SafeAreaView style={styles.box}>
@@ -63,8 +96,9 @@ export default function App() {
             <Text style={styles.titulo}>Criar tarefa</Text>
           </View>
 
-          {/* Input */}
-          <Animatable.View animation="fadeInUp" useNativeDriver>
+          <Animatable.View
+            animation="fadeInUp"
+            useNativeDriver>
             <Text style={styles.stepName}>Título</Text>
             <TextInput
               style={styles.inputText}
@@ -75,29 +109,50 @@ export default function App() {
             />
           </Animatable.View>
 
-          {/* Botão de adicionar */}
-          <TouchableOpacity style={styles.botaoFalso} onPress={adicionarTask}>
+          <AnimatedButton
+            style={styles.botaoFalso}
+            animation="fadeInUp"
+            useNativeDriver
+            onPress={adicionarTask}
+          >
             <Text style={styles.textoBotao}>Adicionar</Text>
-          </TouchableOpacity>
+          </AnimatedButton>
 
-          {/* Botão de voltar */}
-          <TouchableOpacity style={styles.botaoFalso} onPress={() => setOpenModal(false)}>
+          <AnimatedButton
+            style={styles.botaoFalso}
+            animation="fadeInUp"
+            useNativeDriver
+            onPress={() => setOpenModal(false)}>
             <Text style={styles.textoBotao}>Voltar</Text>
-          </TouchableOpacity>
-
-
+          </AnimatedButton>
         </SafeAreaView>
       </Modal>
 
-      {/* Icone Adicionar */}
-      <AnimatedButton
-        style={styles.iconAdd}
-        animation="bounceInUp"
-        duration={2000}
-        onPress={() => setOpenModal(true)}
-        useNativeDriver>
-        <Ionicons name="ios-add" size={30} color="#171d33" />
-      </AnimatedButton>
+      {task.length > 0 &&
+        <View>
+          <View style={styles.content}>
+            <Text style={styles.titulo}>Lembretes</Text>
+          </View>
+          <FlatList
+            style={styles.lista}
+            showsHorizontalScrollIndicator={false} /* Desabilita a barra de rolagem */
+            data={task} /* Onde vai todos os itens da lista */
+            keyExtractor={(item) => String(item.key)} /* Chave do item */
+            renderItem={
+              ({ item }) => <TaskList data={item} deleteTask={deleteTask} />  /* Como vai ser renderizado o item */
+            }
+          />
+
+          <AnimatedButton
+            style={styles.iconAdd}
+            animation="bounceInUp"
+            duration={2000}
+            onPress={() => setOpenModal(true)}
+            useNativeDriver>
+            <Ionicons name="ios-add" size={30} color="#171d33" />
+          </AnimatedButton>
+        </View>
+      }
 
     </SafeAreaView>
   );
@@ -108,6 +163,59 @@ const styles = StyleSheet.create({
   box: {
     flex: 1,
     backgroundColor: '#171d33'
+  },
+
+  contentInitial: {
+    alignItems: 'center',
+    marginTop: 180
+  },
+
+  welcomeTitulo: {
+    fontSize: 28,
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+
+  welcomeDescricao: {
+    fontSize: 16,
+    color: '#fff',
+    marginBottom: 20,
+    width: 250,
+    textAlign: 'justify'
+  },
+
+  welcomeAdd: {
+    width: 60,
+    height: 60,
+    marginTop: 30,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    }
+  },
+
+  addTask: {
+    width: 150,
+    height: 40,
+    marginTop: 30,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    }
   },
 
   titulo: {
@@ -128,7 +236,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     /* Alinha o botão para baixo da tela */
     right: 25,
-    bottom: 25,
+    top: 700,
     elevation: 2, /* Causa o efeito de sombreamento*/
     zIndex: 9,
     shadowColor: "#000",
